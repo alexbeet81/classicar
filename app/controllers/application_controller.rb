3dff6f72e8be73644359b8456c78c7920baff42b
Alex Pritchard <alexbeet1234@gmail.com>
2021-06-16 13:01:38 +0800

    added configure_permitted_parameters to application controller so that we can use username at sign in and log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  # every user will need to be signed in before they can do a certain action.
   before_action :authenticate_user!
+
+  # add additional username to sign up and sign in
+  def configure_permitted_parameters
+    # new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+
+    # edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
