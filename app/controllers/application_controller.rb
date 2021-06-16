336e706b3b97bb527f847865a17f19324aed5484
Alex Pritchard <alexbeet1234@gmail.com>
2021-06-16 13:02:55 +0800

    generated views for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # every user will need to be signed in before they can do a certain action.
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # add additional username to sign up and sign in
   def configure_permitted_parameters
