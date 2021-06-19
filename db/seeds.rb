d14b78f9c8a42415b8a35f4ef723efe96a9fa8f8
Alex Pritchard <alexbeet1234@gmail.com>
2021-06-19 19:07:15 +0800

    migrated long and lat to car - WARNING: Has to comment out lines as migration was not working. Ask me for more info

--- db/seeds.rb
+++ db/seeds.rb
@@ -79,7 +79,6 @@ classic_cars = [{model: 'Jaguar E-Type', seats: '2', year: '1967', address: '8 M
 
 # address = ['Beijing', 'Shanghai', 'Tianjin', 'Chengdu', 'Shenzhen', 'Guangzhou', 'Chongqing', 'Dongguan']
 
-
 100.times do
   new_user = User.new(
     email: Faker::Internet.email,
