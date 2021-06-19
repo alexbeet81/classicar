de712fdc9eac0cc7637562f85781344ecc13d802
Alex Pritchard <alexbeet1234@gmail.com>
2021-06-19 11:11:24 +0800

    moved show edit and update out of nesting in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'cars#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cars do
-    resources :bookings, only: [:show, :new, :create, :edit, :update]
+    resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:destroy]
+  resources :bookings, only: [:show, :destroy, :edit, :update]
 end
