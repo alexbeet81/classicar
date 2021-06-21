d3c51113e1aa4e54d7d589b15fad427d17ba81ec
Alex Pritchard <alexbeet1234@gmail.com>
2021-06-21 21:35:14 +0800

    added cancelled to bookings, where it should have been all along

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_133116) do
+ActiveRecord::Schema.define(version: 2021_06_21_133433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_133116) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.boolean "cancelled", default: true
     t.index ["car_id"], name: "index_bookings_on_car_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
