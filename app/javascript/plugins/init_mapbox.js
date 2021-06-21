a23ab8d347641d6ecba9921e83b05478ae1f8a44
Alex Pritchard <alexbeet1234@gmail.com>
2021-06-21 12:52:07 +0800

    added markers to init_map.js

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -11,6 +11,12 @@ const initMapbox = () => {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
   }
+  const markers = JSON.parse(mapElement.dataset.markers);
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
+  });
 };
 
 export { initMapbox };
