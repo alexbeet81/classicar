aa60d1a2d434d0f530f84c1321fa29f20ee00669
Alex Pritchard <alexbeet1234@gmail.com>
2021-06-21 13:27:50 +0800

    used hero-car png as placemarker in cars_controller - seems to be rendering twice and I don't know why

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -22,7 +22,7 @@ class CarsController < ApplicationController
         lat: car.latitude,
         lng: car.longitude,
         info_window: render_to_string(partial: "info_window", locals: { car: car }),
-        image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
+        image_url: helpers.asset_url('hero-car')
       }
     end
   end
