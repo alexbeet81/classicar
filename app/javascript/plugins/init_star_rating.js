4bd4890a2024b5b3e86675696f787e634e2b36eb
Alex Pritchard <alexbeet1234@gmail.com>
2021-06-25 19:08:58 +0800

    made it so clicking the stars submits the form - can't hide the submit button

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -4,7 +4,11 @@ import $ from 'jquery';
 
 const initStarRating = () => {
   $('#review_star').barrating({
-    theme: 'css-stars'
+    theme: 'css-stars',
+    onSelect: (value, text, event) => {
+      const form = $("form.review_form"); // We are selecting the form on the page with its class
+      form.submit(); // We submit the form with javascript
+    }
   });
 };
 
